da8226918558e96fddf0a11f83b4ec1896434ad9
Nicu Cristian Taran <cristian.taran.work@gmail.com>
2020-09-21 16:27:31 +0300

    database is created before first request

--- api/api.py
+++ api/api.py
@@ -3,11 +3,17 @@ import time
 from flask import Flask
 
 from configuration.config import app_config
+from database.db import db
 
 app = Flask(__name__)
 launch_env = os.getenv('FLASK_ENV')
 app.config.from_object(app_config[launch_env])
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+db.init_app(app)
 
 @app.route('/time')
 def get_current_time():
