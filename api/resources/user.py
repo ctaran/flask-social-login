7bddf24d96da7bb8ff2322bd1076cb33497ee2a5
Nicu Cristian Taran <cristian.taran.work@gmail.com>
2020-09-23 16:43:06 +0300

    protected User API endpoints

--- api/resources/user.py
+++ api/resources/user.py
@@ -1,5 +1,6 @@
 from models.user import UserModel
 from flask_restful import Resource, reqparse
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 class User(Resource):
     parser = reqparse.RequestParser()
@@ -23,13 +24,15 @@ class User(Resource):
         required=True,
         help="Role field cannot be left blank!"
     )
-    
+
+    @jwt_required   
     def get(self, name):
         user = UserModel.find_by_name(name)
         if user:
             return user.json()
         return {'message':'user not found'}, 404
 
+    @jwt_required
     def post(self):
         data = User.parser.parse_args()
         user = None
@@ -52,6 +55,7 @@ class User(Resource):
 
         return user.json(), 201
 
+    @jwt_required
     def delete(self, name):
         user = UserModel.find_by_name(name)
 
@@ -63,7 +67,8 @@ class User(Resource):
                 return {"message": "An error occurred deleting the user - {}".format(err)}, 500
 
         return {'message':'No user with name - {} - exists'.format(name)}, 404
-        
+
+    @jwt_required 
     def put(self, name):
         data = User.parser.parse_args()
         user = UserModel.find_by_name(name)
@@ -87,5 +92,6 @@ class User(Resource):
 
 class UserList(Resource):
 
+    @jwt_required
     def get(self):
         return {"users": [user.json() for user in UserModel.get_all_users()]}
